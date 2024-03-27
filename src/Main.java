e6dbc2bbe619d603e2fe1b22dafe62a33b360255
admin <admin@MacBook-Air-cua-admin.local>
2024-03-27 14:01:44 +0700

    ..

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        double[] myList = new double[10];
+        //myList[9] = 9.0;
+        System.out.println(myList[9]);
+    }
+}
